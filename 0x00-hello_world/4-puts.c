d765ab878d3ae613227c20ad15c99803c949bf41
horlahuk <olaitan.u.k@gmail.com>
2023-02-16 09:00:01 -0800

    added 4file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints sententence using puts
+ * return: (0)Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+return (0);
+}
