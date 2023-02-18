fd1adc72cbf898802073d906b9e6de9d190d1f26
horlahuk <olaitan.u.k@gmail.com>
2023-02-17 20:35:22 -0800

    made changes

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,13 @@
  * Return: (0) Success
  */
 int main(void)
-{
+{	
+	char alpha[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
 
-	for (i = 122; i < 96; i--)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(i);
+		putchar(alpha[i]);
 	}
 	putchar('\n');
 	return (0);
