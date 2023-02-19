0e26d9c9b0d93ba9986877bf5ae4b505078d4e8f
horlahuk <olaitan.u.k@gmail.com>
2023-02-19 04:01:33 -0800

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - printing all possible different combinations of two digits
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int i, x;
+
+	i = 48;
+	x = 48;
+
+	while (x < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (x != i && x < i)
+			{
+				putchar(x);
+				putchar(i);
+				if (i == 57 && x == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++
+		}
+		x++
+	}
+	putchar('\n');
+	return (0);
+}
