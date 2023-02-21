f5ff39dfa50d21e01eea136cf5c4406e1ca9ac1f
horlahuk <olaitan.u.k@gmail.com>
2023-02-21 07:11:05 -0800

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - program to print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int arr[] = {95, 112, 117, 116, 99, 104, 97, 104};
+	int spell, size;
+
+	size = sizeof(arr) / sizeof(int);
+	for (spell = 0; spell < size; spell++)
+	{
+		_putchar(arr[spell]);
+	}
+	_putchar('\n');
+	return (0);
+}
