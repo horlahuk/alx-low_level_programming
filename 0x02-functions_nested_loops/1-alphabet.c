970db09bef9263db32550ee1c31313d7d92a3a98
horlahuk <olaitan.u.k@gmail.com>
2023-02-21 07:49:06 -0800

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * print_alphabet - prints lower case letters
+ *
+ * Return: Always (0) Success
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
