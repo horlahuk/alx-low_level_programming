ae409a3c1eea08ec99008e0613badfe16a050ec5
horlahuk <olaitan.u.k@gmail.com>
2023-02-23 01:11:35 -0800

    made some fixes to code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * positive_or_negative - tests function that prints if integer
- * is positive or negative
- * @i: integer value for i
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
