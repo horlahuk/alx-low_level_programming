86064c38e3b869c76ae72e187d0238d68a31c14c
horlahuk <olaitan.u.k@gmail.com>
2023-02-23 01:02:40 -0800

    added main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests function that prints if integer
+ * is positive or negative
+ * @i: integer value for i
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
