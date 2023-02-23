f7e5fbba81c15bffac43b3884eaed6a71686ec0f
horlahuk <olaitan.u.k@gmail.com>
2023-02-23 03:57:35 -0800

    added mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - prints multiplication of a and b
+ * @a: integer a
+ * @b: integer b
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
