5b50fadda71c84637c03bec04ce39333c461fe47
horlahuk <olaitan.u.k@gmail.com>
2023-02-23 05:06:28 -0800

    added print line file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints straight line n times
+ * @n: n times straight line is printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(95);
+	}
+	-putchar('\n');
+}
