2f0f58b09e61b3693812c3d1661fb6d618899433
horlahuk <olaitan.u.k@gmail.com>
2023-03-02 05:35:50 -0800

    fixed code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ char *leet(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		for (i = 0; i < 5; i++;)
+		for (i = 0; i < 5; i++)
 		{
 			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 			{
