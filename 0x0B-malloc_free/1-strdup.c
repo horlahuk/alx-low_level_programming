a97dccc701a824d98e6ddd19f5a9d5914e605d76
horlahuk <olaitan.u.k@gmail.com>
2023-03-14 07:10:17 -0700

    added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * getLen - gets the length of a string
+ * @s: string to get length
+ * Return: length of string
+ */
+int getLen(char *s)
+{
+	int count = 0;
+
+	while (s != 0)
+	{
+		count = count + 1;
+		s = s + 1;
+	}
+	return (count);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ * @str: string to be copied
+ * Return: pointer to string copied
+ */
+char *_strdup(char *str)
+{
+	int len = getLen(str);
+	char *new, *saved;
+
+	new = malloc(sizeof(char) * (len + 1));
+	if (new == 0)
+		return (NULL);
+	saved = new;
+
+	while (*str != '\0')
+	{
+		new = str;
+		new++;
+		str++;
+	}
+	new = '\0';
+	return (saved);
+}
