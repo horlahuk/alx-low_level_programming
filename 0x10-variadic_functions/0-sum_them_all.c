d8bde153272eca6c8b06bc607f49257a3fb6c0d0
horlahuk <olaitan.u.k@gmail.com>
2023-03-24 03:42:08 -0700

    fixed code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,15 +8,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list sum;
+	va_list list;
 	unsigned int i;
 	int sum = 0;
 
-	va_start(sum, n);
+	va_start(list, n);
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg(sum, int);
-	va_end(sum);
+		sum += va_arg(list, int);
+	va_end(list);
 	return (sum);
 }
