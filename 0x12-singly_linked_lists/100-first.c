4d9b6f431914cc1b7fa6facd7c642ce7d4dd0191
horlahuk <olaitan.u.k@gmail.com>
2023-03-30 18:14:07 -0700

    added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__ ((constructor));
+
+/**
+ * print_before_main - prints things before main
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
