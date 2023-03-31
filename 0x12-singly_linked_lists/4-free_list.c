27c9fc4b86e681df7f8d15e5f5da169753a69819
horlahuk <olaitan.u.k@gmail.com>
2023-03-30 18:06:22 -0700

    added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp->str);
+		free(temp);
+	}
+}
