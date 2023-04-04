2d64998d522e303ce712fdecfec497b0dba7fade
horlahuk <olaitan.u.k@gmail.com>
2023-04-04 03:03:43 -0700

    added 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: start of list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp->n);
+		free(temp);
+	}
+}
