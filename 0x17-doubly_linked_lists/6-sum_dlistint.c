136a132514c014f8c19a5768ed29a7fdfb742d7f
horlahuk <olaitan.u.k@gmail.com>
2023-05-18 07:04:13 -0700

    add 6-sum_dlistint.c file

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of all the data (n) of a dlistint_t linked list
+ * @head: head of the linked list
+ *
+ * Return: sum of all the data (n) of a dlistint_t linked list or 0 if 
+ * list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		count += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
