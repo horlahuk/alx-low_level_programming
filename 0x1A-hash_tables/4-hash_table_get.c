10a661cd4f68965a41e93dc8d542c388071c5fbc
horlahuk <olaitan.u.k@gmail.com>
2023-06-09 11:22:37 -0700

    add 4-hash_table_get.c file

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a value associated with a key
+ * @ht: hash table
+ * @key: key to find
+ *
+ * Return: value of associated element or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+
+		node = node->next;
+	}
+
+	return (NULL);
+}
