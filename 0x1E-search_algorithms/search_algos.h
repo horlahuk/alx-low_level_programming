4322b6a052ed759c009a9289d498846e140c56fb
horlahuk <olaitan.u.k@gmail.com>
2023-09-13 05:22:27 -0700

    advanced tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -23,7 +23,7 @@ typedef struct listint_s
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-nt binary_recursion(int *array, int value, size_t start, size_t end);
+int binary_recursion(int *array, int value, size_t start, size_t end);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
